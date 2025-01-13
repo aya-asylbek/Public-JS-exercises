84cc3644ba0c6a5a04492811d18aece4d59b4207
Aya Asylbek <aichurek0990@gmail>
2025-01-13 01:37:57 -0800

    Completed exercise 1.

--- 3_LoopsExercise.js
+++ 3_LoopsExercise.js
@@ -11,7 +11,13 @@
 
 // Exercise 1. Write a 'while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
-
+function exercise1() {
+  let i = 1;
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
+}
 // Exercise 2. Write a 'do while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
 
