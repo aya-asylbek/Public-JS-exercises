ec80cb8fb05411e3d79edfac85ec491df9e9d136
Aya Asylbek <aichurek0990@gmail>
2025-01-13 21:00:51 -0800

    Completed exercise 1.

--- 8_ObjectsChallenges.js
+++ 8_ObjectsChallenges.js
@@ -18,7 +18,15 @@
 // keys(obj2); // ["first", "last"]
 // let obj3 = {};
 // keys(obj3); // []
-
+function keys(obj) {
+    let result = [];
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push(key);
+      }
+    }
+    return result;
+  }
 // Exercise 2. Write a function called values, which accepts an object and returns an array of all of the values in the object.
 // IMPORTANT: Do not use the built in Object.values() function!
 // Examples:
