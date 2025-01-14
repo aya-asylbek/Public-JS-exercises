96e4b6fa4f0bf128a28de6bdc208853e3995d812
Aya Asylbek <aichurek0990@gmail>
2025-01-13 20:52:09 -0800

    Completed exercise 5.

--- 7_FunctionsChallenges.js
+++ 7_FunctionsChallenges.js
@@ -72,7 +72,14 @@ function removeFromString(str, startIndex, numCharacters) {
 // indexOf(arr2, 2); // --> 1
 // let arr3 = [1, 2];
 // indexOf(arr3, 10); // --> -1
-
+function indexOf(arr, num) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === num) {
+        return i;
+      }
+    }
+    return -1;
+  }
 // Exercise 6. Write a function called includes which accepts a collection, a value, and an optional starting index. The function should return true if the value exists in the collection when we search starting from the starting index. Otherwise, it should return false.
 // The collection can be a string, an array, or an object. If the collection is a string or array, the third parameter is a starting index for where to search from. If the collection is an object, the function searches for the value among values in the object; since objects have no sort order, the third parameter is ignored.
 // Examples:
