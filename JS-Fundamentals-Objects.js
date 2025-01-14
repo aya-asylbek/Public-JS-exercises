2eacfbf8a64fc0783fb24476ee01a37ce458a469
Aya Asylbek <aichurek0990@gmail>
2025-01-13 22:27:30 -0800

    Completed 3rd task of JS Fundamentals Objects.

--- JS-Fundamentals-Objects.js
+++ JS-Fundamentals-Objects.js
@@ -40,6 +40,18 @@ function isEmpty(obj) {
 // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
 // If salaries is empty, then the result must be 0.
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+let sum = 0;
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+alert(sum);
 
 //4.Multiply numeric property values by 2
 //Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
